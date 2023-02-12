90348e0f610b53d38f9f3b59d5de1bb9317f75ab
Mara-Luciana Belu <maraa.lucianaa@yahoo.com>
2023-02-12 13:33:24 +0200

    small change

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import StateProvider from './context';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <StateProvider>
-      <App />
-    </StateProvider>
+    <App />
   </React.StrictMode>
 );
--- Frontend/src/routes/Flowers.tsx
+++ Frontend/src/routes/Flowers.tsx
@@ -1,13 +1,8 @@
 import { Box } from "@mui/material";
 import { CardsFlowers } from "../components/Cards";
 
-import { useTodoActions, useTodoData } from '../context';
-
 
 const Flowers: React.FC = () => {
-    const items = useTodoData()
-    const { editTodo } = useTodoActions()
-
     return (
         <Box>
             <br></br>
